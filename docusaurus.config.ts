766aba154dd267a3a31582a63a35002431692d79
国服第一厉害 <2380385960@qq.com>
2025-07-17 10:30:20 +0800

    fix: site name config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Carrie Fish',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Carrie Fish',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
